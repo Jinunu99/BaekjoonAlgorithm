51e0af3ed45114134058875d7503ee3f44a46269
Jinunu99 <114578011+Jinunu99@users.noreply.github.com>
2023-11-19 13:49:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// 9095번
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	
+	int dp[1001] = { 0 };
+
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
