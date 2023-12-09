8f35914e032803f36b4d93c87a497ec7c6749627
Jinunu99 <114578011+Jinunu99@users.noreply.github.com>
2023-12-09 13:39:33 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <stdio.h>
+using namespace std;
+
+int main(void)
+{
+	priority_queue<int> pq;
+
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		scanf("%d", &x);
+		if (x == 0)
+		{
+			if (pq.empty())
+				printf("0\n");
+			else
+			{
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(x);
+		}
+	}
+
+	return 0;
+}
